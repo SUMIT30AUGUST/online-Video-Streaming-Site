59ea3088b052144041ffd7433551f51883709bf1
unknown <sumitrawat30aug@gmail.com>
2024-03-26 12:18:50 +0530

    video binary data ready to serve to the nodejs server

--- public/script.js
+++ public/script.js
@@ -1,4 +1,32 @@
 const userVideo= document.getElementById('user-video')
+const startButton = document.getElementById('start-btn');
+
+const state = {
+       media:null 
+}
+
+startButton.addEventListener('click',()=>{
+
+   //High Bit rate can puts more load on the server CPU
+   const mediaRecorder = new MediaRecorder(state.media,{
+       // mimeType: 'video/webm;codecs=vp9',
+       audioBitsPerSecond:128000,
+       videoBitsPerSecond:2500000,
+       framerate:25
+   })
+   
+   
+   mediaRecorder.ondataavailable = (ev)=>{
+       console.log('Binary Stream Available', ev.data)
+   }
+   
+
+   /*
+    MediaRecorder will capture and encode chunks of media data
+    from the stream every 25 milliseconds.
+   */
+   mediaRecorder.start(25)
+})
 
 window.addEventListener('load',async(e)=>{
        
@@ -6,7 +34,13 @@ window.addEventListener('load',async(e)=>{
        .mediaDevices
        .getUserMedia({audio:true,video:true})
 
-
+       
+       
        //users own media
        userVideo.srcObject=media
+
+       
+       //state.media is a mediaStream parameter passed to 
+       //MediaRecorder function 
+       state.media=media
 })
